1c47607e3f867e1a5527a1917c95464b43393149
tu <nguyenmanhtu09042000@gmail.com>
2020-05-09 01:36:36 +0700

    update root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root "pets#index"
   resources :listings
   resources :pets
   resources :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
